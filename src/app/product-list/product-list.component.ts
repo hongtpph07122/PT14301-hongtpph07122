68017e2a1ab2c8a6f9eb3fc26bbe60934433b359
hongtpph07122 <tranphuchong1435@gmail.com>
2020-03-17 21:09:59 +0700

    2 files modified, 1 added, 1 deleted

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { data } from '../MockProduct';
+import { Product } from '../Product';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -11,5 +12,13 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  products = data;
+  selected: Product;
+  removeItem(id){
+    this.products = this.products.filter(product => product.id != id)
+  }
+  copyItem(product1){
+    this.selected = product1;
+    console.log(this.selected.name)
+  }
 }
