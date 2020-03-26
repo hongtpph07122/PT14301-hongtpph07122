d39862a0182cb867256f1975c0faccd2901a578f
hongtpph07122 <tranphuchong1435@gmail.com>
2020-03-26 21:17:00 +0700

    1 file modified

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs'
 })
 export class ProductService {
 api = "https://5e7c54c4a917d700166831cf.mockapi.io/product";
+
   constructor(
     private http: HttpClient
   ) { }
